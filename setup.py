68f830cafdff92f23a116e344df9f139e0a896ef
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2024-10-07 17:35:45 +0800

    Add MIT License

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="tagalog-checker",
     version="0.1",
-    description="A simple package to check if a file contains common Tagalog terms.",
-    author="Christan Shane Plaza",
+    description="A simple package to check if a file contains Tagalog terms.",
+    author="Your Name",
+    license="MIT",
     packages=find_packages(),
-    package_data={
-        "tagalog_checker": ["tagalog_terms.json"]  # Include the JSON file
-    },
+    package_data={"tagalog_checker": ["tagalog_terms.json"]},
     install_requires=[],
     entry_points={
         "console_scripts": ["tagalog-checker=tagalog_checker.checker:check_file_for_tagalog_terms"]
